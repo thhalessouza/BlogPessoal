a00c7a2429cba09498ce919404fe93341570603b
thhalessouza <thais-ssouza@outlook.com>
2021-09-29 23:50:30 -0300

    consumo de api parte 1

---
+++ src/app/model/Tema.ts
@@ -0,0 +1,11 @@
+import { Postagens } from "./Postagens"
+
+
+export class Tema {
+
+    public id: number
+    public descricao: string 
+    public postagens: Postagens[]
+
+
+}
---
+++ src/app/model/Usuario.ts
@@ -0,0 +1,16 @@
+import { Postagens } from "./Postagens"
+
+
+
+
+export class Usuario {
+public id: number 
+public nome: string
+public usuario: string
+public senha: string 
+public foto: string
+public tipo: string 
+public dataNascimento: Date 
+public postagens: Postagens[]
+
+}
